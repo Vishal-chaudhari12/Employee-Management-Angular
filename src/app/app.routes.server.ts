3aba815c9537b8b8415f9c1c0f02e2b35865959e
Vishal <vishalchaudhari4530@gmail.com>
2025-04-11 14:12:23 +0530

    solve deployment problem for prerender

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,22 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'login', renderMode: RenderMode.Prerender },
+  { path: 'register', renderMode: RenderMode.Prerender },
+  { path: 'forgot-password', renderMode: RenderMode.Prerender },
+  { path: 'reset-password', renderMode: RenderMode.Prerender },
+  { path: 'otp-verification', renderMode: RenderMode.Server },
+
+  // Add all other routes here:
+  { path: 'admin-profile', renderMode: RenderMode.Server },
+  { path: 'emp-profile', renderMode: RenderMode.Server },
+  { path: 'admin-dashboard', renderMode: RenderMode.Server },
+  { path: 'employee-dashboard', renderMode: RenderMode.Server },
+  { path: 'employee-list', renderMode: RenderMode.Server },
+  { path: 'add-employee', renderMode: RenderMode.Server },
+  { path: 'checkIn', renderMode: RenderMode.Server },
+  { path: 'leave-employee', renderMode: RenderMode.Server },
+  { path: 'employee-leave-form', renderMode: RenderMode.Server },
+  { path: 'side-bar', renderMode: RenderMode.Server },
 ];
